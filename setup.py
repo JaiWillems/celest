e33051aff689a2f48378d67d5f2098f40a95e426
JaiWillems <60400810+JaiWillems@users.noreply.github.com>
2021-02-21 01:21:19 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="SatPy", # Replace with your own username
+    name="Celest", # Replace with your own username
     version="0.0.2",
     author="Jai Willems",
     author_email="jai52h@hotmail.com",
