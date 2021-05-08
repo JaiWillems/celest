735b81af76c260548dfcc6a310685b26fb6c9e70
Jai Willems <60400810+JaiWillems@users.noreply.github.com>
2021-05-08 16:12:46 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="Celest", # Replace with your own username
-    version="1.1.0",
+    version="1.1.6",
     author="Jai Willems",
     author_email="jai52h@hotmail.com",
     description="Satellite orbital position representation library.",
@@ -18,5 +18,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    include_package_data=True,
+    package_data={'': ['data/*.bsp']},
     python_requires='>=3.6',
 )
