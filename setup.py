d073879368c014d3862e6806000366f0a9dc9665
JaiWillems <jai52h@hotmail.com>
2021-05-23 11:01:56 -0600

    Configuring environment and setup requirements.

--- setup.py
+++ setup.py
@@ -12,6 +12,14 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/JaiWillems/Celest",
+    license="MIT",
+    install_requires=[
+        "DateTime",
+        "jplephem",
+        "julian",
+        "numpy",
+        "pandas"
+    ]
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
