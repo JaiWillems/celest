9c2f9250e48f2d334e85b35bf2f73efbdc4678e5
Jai Willems <jai52h@hotmail.com>
2021-07-14 17:41:00 -0600

    DEV: Finalize the `moon.py` file.

--- celest/astronomy/moon.py
+++ celest/astronomy/moon.py
@@ -1,4 +1,9 @@
-"""
+"""Determine Moon positional and special event information.
+
+The astronomy module contains the `Moon` class to access positional and special
+event information. All time and positional representations are returned as
+`Time` and `Coordinate` objects to provide the flexibility of different data
+representaitons.
 """
 
 from celest.core.decorators import set_module
@@ -10,33 +15,98 @@ import numpy as np
 
 @set_module('celest.astronomy')
 class Moon(CelestialObject):
-    """
+    """Determine Moon positional and special event information.
+    
+    Methods
+    -------
+    position(timeData)
+        Return the Moon's position.
+    rise(timeData, groundPos)
+        Return the lunar rise times.
+    set(timeData, groundPos)
+        Return the lunar set times.
+    peak(timeData, groundPos)
+        Return the lunar peak times.
     """
     
     def __init__(self):
+        """Initialize attributes."""
+
         super().__init__()
-
-        self._pos_data = None
-
-        pass
     
     def position(self, timeData: Time) -> Coordinate:
-        pass
+        """Return the Moon's position.
+
+        Parameters
+        ----------
+        timeData : Time
+            Find the positions corresponding to the imputted times.
+        
+        Returns
+        -------
+        Coordinate
+            Position of Mercury at times, `timeData`, as a `Coordinate` object.
+        """
+
+        eb2m = self.kernal[3, 301].compute(timeData.julian)
+        eb2e = self.kernal[3, 399].compute(timeData.julian)
+        e2m = (eb2m - eb2e).T
+
+        return Coordinate(basePos=e2m, type="ECI", timeData=timeData)
     
     def rise(self, timeData: Time, groundPos: GroundPosition) -> np.array:
+        """Return the lunar rise times.
+
+        Parameters
+        ----------
+        timeData : Time
+            Find the rise times encapsulated within the imputted times.
+        groundPos : GroundPosition
+            Position of the observer.
+        
+        Returns
+        -------
+        Time
+            `Time` object containing rise times.
+        """
+        
+        posData = self.position(timeData)
         return self._find_rise(posData, timeData, groundPos)
     
     def set(self, timeData: Time, groundPos: GroundPosition) -> np.array:
+        """Return the lunar set times.
+
+        Parameters
+        ----------
+        timeData : Time
+            Find the set times encapsulated within the imputted times.
+        groundPos : GroundPosition
+            Position of the observer.
+        
+        Returns
+        -------
+        Time
+            `Time` object containing set times.
+        """
+        
+        posData = self.position(timeData)
         return self._find_set(posData, timeData, groundPos)
     
     def peak(self, timeData: Time, groundPos: GroundPosition) -> np.array:
+        """Return the lunar peak times.
+
+        Parameters
+        ----------
+        timeData : Time
+            Find the peak times encapsulated within the imputted times.
+        groundPos : GroundPosition
+            Position of the observer.
+        
+        Returns
+        -------
+        Time
+            `Time` object containing peak times.
+        """
+        
+        posData = self.position(timeData)
         return self._find_peak(posData, timeData, groundPos)
-    
-    def phase(self, timeData: Time, groundPos: GroundPosition) -> np.array:
-        pass
-    
-    def full_moon(self, timeData: Time, groundPos: GroundPosition) -> np.array:
-        pass
-    
-    def lunar_eclipse(self, timeData: Time, groundPos: GroundPosition) -> np.array:
-        pass
