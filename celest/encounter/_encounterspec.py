6ee37a5e9b500e1f9559571bea22fb70f184dced
Jai Willems <jai52h@hotmail.com>
2021-09-07 19:18:23 -0400

    DEV: Remove encounter info and str representation.

--- celest/encounter/_encounterspec.py
+++ celest/encounter/_encounterspec.py
@@ -7,8 +7,6 @@ encounter information specific to a `GroundPosition` Object.
 
 from celest.core.decorators import set_module
 from typing import Literal
-import numpy as np
-import pandas as pd
 
 
 @set_module('celest.encounter')
@@ -25,12 +23,12 @@ class EncounterSpec(object):
     encType :  {"I", "T"}
         Specifies encounter category as either imaging or transmission.
     ang : float
-        Angluar constraint for the encounter in degrees.
+        Angular constraint for the encounter in degrees.
     angType : {"A", "N"}
-        String specifying the constraint angle as either the altitude, or
+        String specifying the constraint angle as either the altitude or
         off-nadir angle type. Refer to notes for greater detail in the angle
         definitions.
-    SCA : float, optional
+    sca : float, optional
         Float specifying the minimum angle between a satellite's position
         vector and the Sun's position vector in a ground-location-centric
         reference system. Refer to notes for more information.
@@ -45,9 +43,9 @@ class EncounterSpec(object):
     type : {"I", "T"}
         Specifies encounter category as either imaging or transmission.
     ang : float
-        Angluar constraint for the encounter in degrees.
+        Angular constraint for the encounter in degrees.
     ang_type : {"A", "N"}
-        String specifying the constraint angle as either the altitude, or the
+        String specifying the constraint angle as either the altitude or the
         off-nadir angle type.
     solar : {-1, 0, 1}, optional
         Defines sunlight allowance where -1 allows for windows at night, 0
@@ -56,33 +54,27 @@ class EncounterSpec(object):
         Float specifying the minimum angle between a satellite's position
         vector and the Sun's position vector in a ground-location-centric
         reference system. Refer to notes for more information.
-    windows : np.array
-        Array of shape (n, 3) of window start, end, and elapsed seconds data.
-    length : int
-        Length, n, of data attributes.
-    encounter_indices : np.array
-        Array containing arrays of encounter indices.
-    
+
     Notes
     -----
     The altitude angle is defined as the angle of a celestial object measured
     in increasing degrees from the horizon. A valid encounter region with the
     altitude angle are the regions where `alt_angle > ang`. The off-nadir
-    angle is defined as the angle of an Earth based location measured in
+    angle is defined as the angle of an Earth-based location measured in
     increasing degrees from a satellite's nadir. A valid encounter region with
     the off-nadir angle are the regions where `nadir_ang < ang`.
 
     In some instances of ground to satellite communication, hardware damage
     can be incurred when the ground station is within a certain angle of the
-    sun. The solar constraint angle allows encounters to be calculated out of
-    direct alignment of the sun by invalidating encounter regions where the
-    sun is behind or close to the satellite as seen from the ground station
+    Sun. The solar constraint angle allows encounters to be calculated out of
+    direct alignment of the Sun by invalidating encounter regions where the
+    Sun is behind or close to the satellite as seen from the ground station
     assuming the ground station is actively tracking the satellite.
     """
-    
+
     def __init__(self, name: str, encType: Literal["I", "T"], ang: float,
                  angType: Literal["A", "N"], solar: Literal[-1, 0, 1]=0,
-                 SCA: float=0) -> None:
+                 sca: float=0) -> None:
         """Initialize attributes."""
 
         self.name = name
@@ -90,18 +82,4 @@ class EncounterSpec(object):
         self.ang = ang
         self.ang_type = angType
         self.solar = solar
-        self.solar_constraint_ang = SCA
-        self.windows = None
-        self.length = None
-        self.encounter_indices = None
-    
-    def __str__(self) -> str:
-        """Define `EncounterSpec` informaiton string."""
-
-        data = np.array([self.name, self.type, self.ang, self.ang_type, self.solar])
-        index = np.array(["Name:", "Encounter Type:", "Constraint Angle:",
-                          "Angle Type:", "Solar Allowance:", "Solar Constraint Angle:"])
-
-        df = pd.DataFrame(data=data, index=index)
-
-        return df.to_string()
+        self.solar_constraint_ang = sca
