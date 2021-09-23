a872553ab84001b5704f2680a617d6b6a9d26cb6
Nishkrit Desai <nishkritdesai10@gmail.com>
2021-09-23 14:49:49 -0400

    Removed stray window module import

--- celest/encounter/__init__.py
+++ celest/encounter/__init__.py
@@ -1,4 +1,3 @@
 from celest.encounter._encounter_utils import analytical_encounter_ind
-from celest.encounter._windowhandling import Window, WindowList
 from celest.encounter.encounter import Encounter
 from celest.encounter.groundposition import GroundPosition
