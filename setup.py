856bfd22a561f9dd6f87833b560381503ae63b81
Jai Willems <jai52h@hotmail.com>
2022-04-29 18:06:15 -0400

    REL: Update `setup.py` for 0.3.0 release.

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
+"""Celest is a package for satellite mission planning.
+
+It provides:
+
+- Diverse arrangement of time and coordinate conversions,
+- Satellite orbital and attitude calculations, and
+- Satellite-to-ground encounter calculations.
+
+Refer to project links for documentation and source code urls.
+
+All Celest wheels distributed on PyPI are BSD licensed.
+"""
+DOCLINES = (__doc__ or '').split("\n")
+
+
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+
+__version__ = "0.3.0"
+
 
 setuptools.setup(
     name="celest",
-    version="0.2.0",
+    version=__version__,
     author="Jai Willems et al.",
     author_email="jai52h@hotmail.com",
     description="Satellite dynamics and mission planning library.",
-    long_description=long_description,
+    long_description="\n".join(DOCLINES),
     long_description_content_type="text/markdown",
     url="https://github.com/JaiWillems/Celest",
     download_url="https://pypi.org/project/celest/",
