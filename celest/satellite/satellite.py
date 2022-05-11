a9344a524b11293534512513198750800a67fcb7
Jai Willems <jai52h@hotmail.com>
2022-05-11 13:25:13 -0400

    MAINT: Add velocity input to 'Satellite` class.

--- celest/satellite/satellite.py
+++ celest/satellite/satellite.py
@@ -5,15 +5,12 @@ import pandas as pd
 
 
 class Satellite(Coordinate):
-    """Satellite(position, frame, julian, offset=0)
+    """Satellite(position, velocity, frame, julian, offset=0)
 
     Satellite abstraction for satellite-ground encounters.
 
     `julian + offset` is the Julian time in the J2000 epoch associated with
-    input positions. If `frame=="geo"`, the `position` input can have 2
-    columns (latitude, longitude) or three columns (latitude, longitude,
-    altitude). If no geodetic altitude is provided, it is assumed zero. Other
-    frames require three columns.
+    input positions.
 
     Parameters
     ----------
@@ -21,8 +18,11 @@ class Satellite(Coordinate):
         2-D array containing position coordinates.
 
         Supported coordinate frames include the Geocentric Celestial Reference
-        System (gcrs), International Terrestrial Reference System (itrs), and
-        geographical (geo) system.
+        System (gcrs) and International Terrestrial Reference System (itrs).
+    velocity : array_like
+        2-D array containing velocity coordinates.
+
+        Refer to the `position` parameter for supported coordinate frames.
     frame : {"gcrs", "geo", "itrs"}
         Frame specifier for the input position.
     julian : array_like
@@ -32,20 +32,23 @@ class Satellite(Coordinate):
         in to add to the julian times.
     offset : float, optional
         Offset to convert input time data to the J2000 epoch, default is zero.
-    
+
     Examples
     --------
-    Initialize `Satellite` using gcrs positions:
+    Initialize `Satellite` using gcrs data:
 
-    >>> julian = [30462.50, 30462.50]
-    >>> position = [[-4681.50824149 -5030.09119386     0.        ]
-    ...             [-4714.35351825 -4978.74325953   454.41492765]]
-    >>> s = Satellite(position=position, frame="gcrs", julian=julian, offset=0)
+    >>> julian = [30462.50000, 30462.50069]
+    >>> position = [[-4681.50824, -5030.09119, 000.00000]
+    ...             [-4714.35352, -4978.74326, 454.41493]]
+    >>> velocity = [[-0.72067, 0.67072, 7.57919]
+    ...             [-0.37378, 1.04024, 7.56237]]
+    >>> s = Satellite(position=position, velocity=velocity, frame="gcrs",
+    ...               julian=julian, offset=2430000)
     """
 
-    def __init__(self, position, frame, julian, offset=0) -> None:
+    def __init__(self, position, velocity, frame, julian, offset=0) -> None:
 
-        super().__init__(position, frame, julian, offset)
+        super().__init__(position, velocity, frame, julian, offset)
 
     def __len__(self) -> int:
 
@@ -53,7 +56,7 @@ class Satellite(Coordinate):
 
     def save(self, times=("julian",), positions=("gcrs",), path=None,
              sep=",", float_format=None) -> None:
-        """Save satellite data.
+        """Export satellite data.
 
         Parameters
         ----------
@@ -85,10 +88,20 @@ class Satellite(Coordinate):
             "ut1": (self.ut1, "UT1.hr"),
             "gmst": (self.gmst, "GMST.hr"),
             "gast": (self.gast, "GAST.hr"),
-            "itrs": (self.itrs, "ITRS.x.km", "ITRS.y.km", "ITRS.z.km"),
-            "gcrs": (self.gcrs, "GCRS.x.km", "GCRS.y.km", "GCRS.z.km"),
+            "itrs": (self.itrs, "ITRS.x.km", "ITRS.y.km", "ITRS.z.km",
+                     "ITRS.vx.m/s", "ITRS.vy.m/s", "ITRS.vz.m/s"),
+            "gcrs": (self.gcrs, "GCRS.x.km", "GCRS.y.km", "GCRS.z.km",
+                     "GCRS.vx.m/s", "GCRS.vy.m/s", "GCRS.vz.m/s"),
             "geo": (self.geo, "GEO.lat.deg", "GEO.lon.deg", "GEO.height.km")
         }
+
+        for time in times:
+            if time not in key_mapping:
+                raise ValueError(f"{time} is not a valid time representation.")
+
+        for position in positions:
+            if position not in key_mapping:
+                raise ValueError(f"{position} is not a valid position representation.")
 
         data = {}
 
